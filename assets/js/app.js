e2c716afdf67911b00b9d9b6cec46186f17d58db
Arsene Kevin <kevinkouomeu@icloud.com>
2020-07-18 17:07:28 +0200

    finsih interface

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,19 @@ class customChart {
   static getClicksEventDn() {
     const canvas_doghnut = document.getElementById("myChartDoghnut");
     canvas_doghnut.onclick = (evt) => {
-      let activePoints = myChart.getElementsAtEvent(evt);
+      let activePoints = myChart_doghnut.getElementsAtEvent(evt);
+      activePoints.length !== 0 ? console.dir(activePoints) : 0;
+    };
+  }
+
+  static getClicksEventLn() {
+    const canvas_line = document.getElementById("myChartLine");
+    canvas_line.onclick = (evt) => {
+      let activePoints = myChart_line.getElementsAtEvent(evt);
       activePoints.length !== 0 ? console.dir(activePoints) : 0;
     };
   }
 }
 
 customChart.getClicksEventDn();
+customChart.getClicksEventLn();
--- assets/js/chart_line.js
+++ assets/js/chart_line.js
@@ -2,30 +2,36 @@ const canvas_line = document.getElementById("myChartLine");
 let myChart_line = new Chart(canvas_line, {
   type: "line",
   data: {
-    labels: [
-      "Hospitalization",
-      "Other",
-      "Life Threatening",
-      "Disabling",
-      "Death",
-    ],
     datasets: [
       {
-        label: "% of all Drug Adverse Events",
-
-        data: [40, 59, 5, 4, 17],
-        backgroundColor: [
-          "rgb(205,10,52)",
-          "rgb(54,162,235)",
-          "rgb(255,206,86)",
-          "rgb(21,134,23)",
-          "rgb(140,198,238)",
-        ],
+        data: [1, 15],
+        backgroundColor: ["transparent"],
+        borderColor: ["green"],
+      },
+      {
+        data: [5, 8],
+        backgroundColor: ["transparent"],
+        borderColor: ["red"],
+      },
+      {
+        data: [2, 11],
+        backgroundColor: ["transparent"],
+        borderColor: ["blue"],
       },
     ],
   },
   options: {
-    // This chart will not respond to mousemove, etc
-    events: ["click"],
+    scales: {
+      yAxes: [
+        {
+          stacked: true,
+        },
+      ],
+      xAxes: [
+        {
+          stacked: true,
+        },
+      ],
+    },
   },
 });
